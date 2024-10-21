9aa90a50b1975b9be02675f8a054d28096984ab9
Sean Murphy <sean@iamseanmurphy.com>
2024-10-21 15:29:43 +0000

    4.7.10

--- button-contact-vr/inc/all-in-one.php
+++ button-contact-vr/inc/all-in-one.php
@@ -9,23 +9,23 @@
         <a href="admin.php?page=contact_vr_setting" class="nav-tab">Setting</a>
     </h2>
 
-    <?php if( isset($_GET['settings-updated']) ): ?>
+    <?php if (isset($_GET['settings-updated'])): ?>
         <div id="message" class="updated">
-            <p><strong><?php _e('Settings saved.'); ?></strong></p>
+            <p><strong><?php esc_html_e('Settings saved.', 'button-contact-vr'); ?></strong></p>
         </div>
     <?php endif; ?>
 
     <form method="post" action="options.php">
         <?php settings_fields('pzf_settings_all_in_one'); ?>
-        <table class="form-table">     
+        <table class="form-table">
             <tr valign="top">
                 <th scope="row">Enable:</th>
                 <td>
-                    <input id="pzf_enable_all_in_one" name="pzf_enable_all_in_one" type="checkbox" value="1" 
-                           <?php checked(get_option('pzf_enable_all_in_one'), '1'); ?> />
+                    <input id="pzf_enable_all_in_one" name="pzf_enable_all_in_one" type="checkbox" value="1"
+                        <?php checked(get_option('pzf_enable_all_in_one'), '1'); ?> />
                 </td>
-            </tr>   
-<!--             <tr valign="top">
+            </tr>
+            <!--             <tr valign="top">
                 <th scope="row">Note:</th>
                 <td>
                     <input placeholder="contact" type="text" name="pzf_note_all_in_one" 
@@ -34,7 +34,7 @@
                     <i>Show on hover</i>
                 </td>
             </tr>         -->
-<!--             <tr valign="top">
+            <!--             <tr valign="top">
                 <th scope="row">Note (show/hide)</th>
                 <td>
                     <input id="pzf_note_bar_all_in_one" name="pzf_note_bar_all_in_one" type="checkbox" value="1" 
@@ -46,7 +46,7 @@
                 <td>
                     <input id="pzf_color_all_in_one" class="my-color-field" name="pzf_color_all_in_one" type="text" value="<?php echo esc_attr(get_option('pzf_color_all_in_one')); ?>" pattern="^#([A-Fa-f0-9]{6})$" title="Color must be in the format #RRGGBB" maxlength="7" />
                 </td>
-            </tr> 
+            </tr>
             <tr valign="top">
                 <th scope="row">Style icon:</th>
                 <td>
@@ -56,41 +56,61 @@
                         $color = get_option('pzf_color_all_in_one') ?: '#2196f3';
                         foreach ($icons as $icon): ?>
                             <li>
-                                <label for="pzf_icon_all_in_one<?php echo $icon; ?>">
+                                <label for="pzf_icon_all_in_one<?php echo esc_attr($icon); ?>">
                                     <span style="background: <?php echo esc_attr($color); ?>">
-                                        <img src="/wp-content/plugins/button-contact-vr/img/icon<?php echo $icon; ?>.png">
+                                        <img src="/wp-content/plugins/button-contact-vr/img/icon<?php echo esc_attr($icon); ?>.png">
                                     </span>
-                                    <input id="pzf_icon_all_in_one<?php echo $icon; ?>" name="pzf_icon_all_in_one" type="radio" value="<?php echo $icon; ?>" 
-                                           <?php checked(get_option('pzf_icon_all_in_one'), $icon); ?> />
-                                    icon <?php echo $icon; ?>
+                                    <input id="pzf_icon_all_in_one<?php echo esc_attr($icon); ?>" name="pzf_icon_all_in_one" type="radio" value="<?php echo esc_attr($icon); ?>"
+                                        <?php checked(get_option('pzf_icon_all_in_one'), $icon); ?> />
+                                    icon <?php echo esc_attr($icon); ?>
                                 </label>
                             </li>
                         <?php endforeach; ?>
-                    </ul>                
+                    </ul>
                 </td>
-            </tr>  
+            </tr>
             <tr valign="top">
                 <th scope="row">Item show/hidden:</th>
                 <td>
-                    <input id="pzf_hide_default_all_in_one" name="pzf_hide_default_all_in_one" type="checkbox" value="1" 
-                           <?php checked(get_option('pzf_hide_default_all_in_one'), '1'); ?> />
+                    <input id="pzf_hide_default_all_in_one" name="pzf_hide_default_all_in_one" type="checkbox" value="1"
+                        <?php checked(get_option('pzf_hide_default_all_in_one'), '1'); ?> />
                     <i>Show</i><br>
                     <i>Default is hidden</i>
                 </td>
-            </tr> 
+            </tr>
         </table>
+
         <?php submit_button(); ?>
     </form>
 
     <hr />
 
-    <h2><?php esc_html_e('Support', 'support_pzf'); ?></h2>
+    <h2><?php esc_html_e('Support', 'button-contact-vr'); ?></h2>
     <p>
-        <?php _e('For submitting any support queries, feedback, bug reports, or feature requests, please visit <a href="https://wordpress.org/plugins/button-contact-vr/" target="_blank">this link</a>.', 'support_pzf'); ?>
+        <?php
+        echo wp_kses(
+            sprintf(
+                // translators: %s will be replaced with the actual link
+                __('For submitting any support queries, feedback, bug reports or feature requests, please visit <a %s>this link</a>.', 'button-contact-vr'),
+                'href="https://wordpress.org/plugins/button-contact-vr/" target="_blank"'
+            ),
+            ["a" => ["href" => [], "target" => []]]
+        );
+        ?>
     </p>
-    <h2><?php esc_html_e('Help', 'support_pzf'); ?></h2>
+    <h2><?php esc_html_e('Help', 'button-contact-vr'); ?></h2>
     <p>
-        <?php _e('Please visit <a href="https://webvocuc.com/blog/tag/button-contact-vr" target="_blank">this link</a>.', 'help_pzf'); ?>
+
+        <?php
+        echo wp_kses(
+            sprintf(
+                // translators: %s will be replaced with the actual link
+                __('Please visit <a %s>this link</a>.', 'button-contact-vr'),
+                'href="https://webvocuc.com/blog/tag/button-contact-vr" target="_blank"'
+            ),
+            ["a" => ["href" => [], "target" => []]]
+        );
+        ?>
     </p>
 </div>
 
@@ -101,24 +121,29 @@
         padding: 0;
         list-style-type: none;
     }
+
     ul#style-icon-vr li {
         display: inline-block;
         margin-bottom: 0;
         margin-right: 15px;
         text-align: center;
     }
+
     ul#style-icon-vr li label {
         display: block;
         cursor: pointer;
     }
+
     ul#style-icon-vr li span {
         display: block;
         width: 30px;
         height: 30px;
         border-radius: 50%;
         position: relative;
-        margin: 0 auto;padding: 8px;
+        margin: 0 auto;
+        padding: 8px;
     }
+
     ul#style-icon-vr li span img {
         max-width: 27px;
         position: absolute;
--- button-contact-vr/readme.txt
+++ button-contact-vr/readme.txt
@@ -5,8 +5,8 @@ Donate link: paypal.me/virustran
 Requires at least: 4.7
 Tested up to: 6.5.2
 Requires PHP: 5.6
-Tested up to: 6.6.1
-Stable Tag: 4.7.9.1
+Tested up to: 6.6
+Stable Tag: 4.7.10
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -128,3 +128,8 @@ Tested up to wordpress: 6.2
 
 = 4.7.9
 * fix num phone
+
+= 4.7.10
+* Bug fixes
+* Better sanitizing
+* Updated translations
